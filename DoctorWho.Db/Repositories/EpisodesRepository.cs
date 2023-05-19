4708f3ef99e26bb1f2d97759f49d4e88da201728
Dana Zagha <dana.zagha0@gmail.com>
2023-05-19 23:50:38 +0300

    step6

--- DoctorWho.Db/Repositories/EpisodesRepository.cs
+++ DoctorWho.Db/Repositories/EpisodesRepository.cs
@@ -9,18 +9,23 @@ namespace DoctorWho.Db.Repositories
 {
     public class EpisodesRepository
     {
-        public static void CreateEpisode(int seriesNumber, int episodeNumber, string episodeType, string title, DateTime episodeDate, int authorId, int doctorId, string notes)
+        public static EpisodesRepository current { get; } = new EpisodesRepository();
+        public void CreateEpisode(int seriesNumber, int episodeNumber, string episodeType, string title, DateTime episodeDate, int authorId, int doctorId, string notes)
         {
             if (title == null) throw new ArgumentNullException("Cannot create an Episode with a null Title!");
             DoctorWhoDbContext.context.Episodes.Add(new Episode { SeriesNumber = seriesNumber, EpisodeNumber = episodeNumber, EpisodeType = episodeType, Title = title, EpisodeDate = episodeDate, AuthorId = authorId, DoctorId = doctorId, Notes = notes });
             DoctorWhoDbContext.context.SaveChanges();
         }
-        public static void UpdateEpisode()
+        public void UpdateEpisode()
         {
             DoctorWhoDbContext.context.ChangeTracker.DetectChanges();
             DoctorWhoDbContext.context.SaveChanges();
         }
-        public static void DeleteEpisode(Episode episode)
+        public List<Episode> GetAllEpisodes()
+        {
+            return DoctorWhoDbContext.context.Episodes.ToList();
+        }
+        public void DeleteEpisode(Episode episode)
         {
             if (episode == null) throw new ArgumentNullException("Cannot remove a null Episode from the Episodes table");
             DoctorWhoDbContext.context.Episodes.Remove(episode);
---
+++ DoctorWho.Web/Controllers/EpisodesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using DoctorWho.Db.Repositories;
+using DoctorWho.Web.Dtos;
+using DoctorWho.Domain;
+using DoctorWho.Db;
+
+namespace DoctorWho.Web.Controllers
+{
+    [Route("api/episodes")]
+    [ApiController]
+    public class EpisodesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public EpisodesController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetEpisodes()
+        {
+            var episodes = EpisodesRepository.current.GetAllEpisodes();
+            var episodesDtos = _mapper.Map<IEnumerable<EpisodeDto>>(episodes);
+
+            return Ok(episodesDtos);
+        }
+    }
+}
